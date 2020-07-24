ad031fb62a84b9da832999ac75d825f6593aa800
Matthew Ritch <mritch3@gatech.edu>
2020-07-24 14:06:23 -0400

    exported model with tf 1.12.0

--- src/main/java/gt/ethier/axonet/AxoNet.java
+++ src/main/java/gt/ethier/axonet/AxoNet.java
@@ -52,7 +52,8 @@ public class AxoNet implements Command {
 		
 		//define model identifiers
 		//explained in https://www.tensorflow.org/api_docs/python/tf/saved_model/simple_save
-		private static final String MODEL_URL = "https://drive.google.com/u/0/uc?export=download&confirm=dL3K&id=18zYR-sNUhY0txKsvLBYbVltssAynN0Jp"; //downloads model from our google drive
+		//private static final String MODEL_URL = "https://drive.google.com/u/0/uc?export=download&confirm=dL3K&id=18zYR-sNUhY0txKsvLBYbVltssAynN0Jp"; //downloads model from our google drive\
+		private static final String MODEL_URL = "https://github.com/ethier-lab/AxoNet-fiji/raw/master/export_model.zip";
 		private static final String MODEL_NAME = "model_1"; 
 		//private static final String MODEL_URL = "https://drive.google.com/u/0/uc?export=download&confirm=NSZm&id=1pjLm13tMq_O7DwUUVw1h8d5vYuOuZAcT"; //downloads model from our google drive
 		//private static final String MODEL_NAME = "model_3"; 
@@ -85,6 +86,7 @@ public class AxoNet implements Command {
 			//display error level to get the log to open
 			log.error("Opening log window...\n");
 			log.log(LogLevel.INFO, "AxoNet is running now!");
+			log.log(LogLevel.INFO, tensorFlowService.getTensorFlowVersion());
 			//load image
 			ImagePlus img = WindowManager.getCurrentImage(); 
 			//warn if no image is loaded
